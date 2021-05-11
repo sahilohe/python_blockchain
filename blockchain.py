a817244fe329d5a12688b147e5111f59f5950108
Sahil Ohe <sahilohe97@gmail.com>
2021-05-11 14:06:30 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,26 +1,19 @@
 import datetime, json, hashlib
-
 #Define the blockchain
 class Blockchain:
-
     def __init__(self):
         self.chain = [self.genesisBlock()]
-        #self.genesisBlock()
 
     def genesisBlock(self):
-
         gBlock = {
             'index' : 0,
-
             'hash' : self.calcHash(str({
                 'index': 0, 
                 'timstamp': datetime.datetime.now(), 
                 'previousHash': '0xx',
                 'data': {'amount ': 500}
                 
-            }))
-                
-                
+            }))               
         }
         #self.chain.append(gBlock)
         return gBlock
@@ -43,7 +36,6 @@ class Blockchain:
         return hashlib.sha256(json.dumps(block, sort_keys=True).encode()).hexdigest()
 
 # Let's test the blockchain
-
 blockchain1 = Blockchain()
 i = 0
 
@@ -52,7 +44,6 @@ while i < 10: # let's write 10 blocks
     i = i + 1
 
 json_data = json.dumps(blockchain1.chain,indent=4) # looks good in json format
-
 #print(json_data) 
 
 #write the output to a file named 'chainData.dat'
